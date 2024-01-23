1b6dff1a065b4e36373987088d99b6722d17dfe6
Hulekal <sachin.hulekal@hp.com>
2024-01-23 17:04:13 +0530

    ip

---
+++ src/main/java/liqui/sac/App.java
@@ -0,0 +1,57 @@
+package liqui.sac;
+
+/**
+ * Hello world!
+ *
+ */
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+import java.sql.*;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.FileSystemResourceAccessor;
+
+public class App {
+	public String handleRequest(Context context) throws SQLException, LiquibaseException {
+		Properties prop = new Properties();
+		try {
+			// load a properties file from class path, inside static method
+			prop.load(App.class.getClassLoader().getResourceAsStream("db.properties"));
+
+			// get the property value and print it out
+			System.out.println(prop.getProperty("url"));
+			System.out.println(prop.getProperty("username"));
+			System.out.println(prop.getProperty("password"));
+
+			String url = prop.getProperty("url");
+			String username = prop.getProperty("username");
+			String password = prop.getProperty("password");
+
+			Connection con = DriverManager.getConnection(url, username, password);
+			System.out.println(con);
+
+			Database database = DatabaseFactory.getInstance()
+					.findCorrectDatabaseImplementation(new JdbcConnection(con));
+			Liquibase liquibase = new Liquibase(
+					"C:\\test\\gLiui\\sac\\src\\main\\resources\\changelog.sql",
+					new FileSystemResourceAccessor(),
+					database);
+			liquibase.update("");
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+}
